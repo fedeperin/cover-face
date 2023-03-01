5aec80960391f9e51f9866e34a7af4fbe8576814
fedeperin <federicoperin@outlook.com.ar>
2023-03-01 18:50:45 -0300

    Go back to principal page when no faces detected

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script>
-    import { imageStatus } from '$lib/stores.js'
+    import { imageStatus, fileSent } from '$lib/stores.js'
     import toast, { Toaster } from 'svelte-french-toast'
     import Nav from '$lib/components/Nav.svelte'
     import Footer from '$lib/components/Footer.svelte'
 
-    imageStatus.subscribe(({ error }) => {
+    imageStatus.subscribe(({ error, status }) => {
+        if(status == 'UPLOADING') {
+            fileSent.set(false)
+        }
+        
         if(error) {
             toast.error(error)
         }
